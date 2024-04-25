8b77a5b2c4aa20418ee00079733d6cc6ce2a3b45
Jorge Cruz <jorgerafaelcruzflores@gmail.com>
2024-04-25 13:42:32 -0600

    seguridad formulario

--- src/main/java/com/tcna/primeraweb/entities/Persona.java
+++ src/main/java/com/tcna/primeraweb/entities/Persona.java
@@ -1,6 +1,10 @@
 package com.tcna.primeraweb.entities;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,7 +20,31 @@ public class Persona {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nombre;
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	private String nombre;
 
     private int edad;
 
